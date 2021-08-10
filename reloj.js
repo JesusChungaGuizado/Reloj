b14cd3a3873f37d855863f7a4a7473f0ba28c246
Jesus Chunga <jesus54248@hotmail.com>
2021-08-09 20:37:23 -0500

    Se agrego funcionamiento en JS

--- reloj.js
+++ reloj.js
@@ -1,28 +1,24 @@
 const mostrarReloj=()=>{
     let fecha=new Date();
-  
-    let h=formatoHora(fecha.getHours());
-    let min=formatoHora(fecha.getMinutes());
-    let seg=formatoHora(fecha.getSeconds());
-    document.getElementById("hora").innerHTML=`${h}:${min}:${seg}`
-    const meses=['Ene','Feb','Mar','Abr','May','Jun','Ago','Sep','Oct','Nov','Dic']
-    const dias=['Dom','Lun','Mar','Mie','Jue','Vie','Sab']
-   
-    let diaSem=dias[fecha.getDay()];
-    let mes = meses[fecha.getMonth()];
-    let dia=fecha.getDate();
-    let year=fecha.getFullYear();
-    
-    document.getElementById("fecha").innerHTML=`${diaSem},${dia} ${mes} del ${year}`;
-    //con el .classList.toggle("estilo"); puedes acceder a los estilos de tu archivo css
-    document.getElementById("contenedor").classList.toggle("animar");
+    let h=formatoNumero(fecha.getHours());
+    let min=formatoNumero(fecha.getMinutes());
+    let seg=formatoNumero(fecha.getSeconds());
+    // document.getElementById("hora").innerHTML=`${h}:${min}:${seg}`
+    // const meses=['Ene','Feb','Mar','Abr','May','Jun','Ago','Sep','Oct','Nov','Dic']
+    // const dias=['Dom','Lun','Mar','Mie','Jue','Vie','Sab']
+    // let diaSem=dias[fecha.getDay()];
+    // let mes = meses[fecha.getMonth()]; 
+    let dia=formatoNumero(fecha.getDate());
+    // let year=fecha.getFullYear();
+    document.getElementById("day").innerHTML=`${dia}`;
+    document.getElementById("hour").innerHTML=`${h}`;
+    document.getElementById("min").innerHTML=`${min}`;
+    document.getElementById("seg").innerHTML=`${seg}`;
 }
 setInterval(mostrarReloj,1000);
-const formatoHora=(hora)=>{
-    if (hora<10) {
-        hora="0"+hora;
+const formatoNumero=(num)=>{
+    if (num<10) {
+        num="0"+num;
     }
-    return hora;
+    return num;
 }
-// let fechas=new Date();
-// console.log(  fechas.getDay)
